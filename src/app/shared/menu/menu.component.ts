09b7fc427c57d065fef8e3919b91afc6c4c7f507
Angel Uzcategui <angeluz142@gmail.com>
2022-07-22 21:30:49 -0300

    agregado de menu

---
+++ src/app/shared/menu/menu.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+
+interface MenuItem{
+  ruta:string,
+  texto:string
+}
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent {
+
+menu:MenuItem[]=[
+  {
+    ruta:'/graficas/barras',
+    texto:'Barras'
+  },
+  {
+    ruta:'/graficas/barras-doble',
+    texto:'Barras Doble'
+  },
+  {
+    ruta:'/graficas/dona',
+    texto:'Dona'
+  },
+  {
+    ruta:'/graficas/dona-http',
+    texto:'Dona Http'
+  }
+]
+
+  constructor() { }
+
+
+
+}
