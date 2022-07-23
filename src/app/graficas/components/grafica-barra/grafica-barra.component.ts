04f39e92e40d80c166464a6414f59d3901b92c0f
Angel Uzcategui <angeluz142@gmail.com>
2022-07-22 23:27:01 -0300

    agregado componente de grafica reutilizable con pase de parametros

--- src/app/graficas/components/grafica-barra/grafica-barra.component.ts
+++ src/app/graficas/components/grafica-barra/grafica-barra.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { ChartConfiguration, ChartData, ChartDataset, ChartType, ChartOptions } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-grafica-barra',
-  templateUrl: './grafica-barra.component.html',
-  styleUrls: ['./grafica-barra.component.css']
+  templateUrl: './grafica-barra.component.html'
 })
 export class GraficaBarraComponent implements OnInit {
 
+
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+
+  public barChartOptions: ChartConfiguration['options'] = {
+    responsive: true  ,
+    indexAxis:'x'
+  };
+  public barChartType: ChartType = 'bar';
+
+  @Input() horizontal:boolean = false;
+  @Input() labels:string[]=[];
+  @Input() dataset:ChartDataset<'bar'>[]=[];
+
+
+  public barChartData: ChartData<'bar'> = {
+    labels:[],
+    datasets:[]
+  } ;
+  
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.horizontal);
+    
+    if(this.horizontal){
+      this.barChartOptions!.indexAxis='y';
+    }
+    
+    this.barChartData.labels = this.labels;
+    this.barChartData.datasets = this.dataset;
   }
 
 }
